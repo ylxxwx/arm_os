d2a8347d2ed55a83d2aecb56bc133abdfe95f9b1
Liping LIU <lipingliu@google.com>
2023-05-19 19:12:39 -0400

    aarch64

--- 01_helloworld/uart_realmode.c
+++ 01_helloworld/uart_realmode.c
@@ -1,5 +1,7 @@
-volatile unsigned int * const UART0_PA = (unsigned int*)0x101f1000;
-volatile unsigned int * const UART1_PA = (unsigned int*)0x101f2000;
+//volatile unsigned int * const UART0_PA = (unsigned int*)0x101f1000;
+volatile unsigned int * const UART0_PA = (unsigned int*)0x09000000;
+
+//volatile unsigned int * const UART1_PA = (unsigned int*)0x101f2000;
 
 void print_uart0(const char *s) {
     while(*s != 0) {
